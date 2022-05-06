a05956408943d9a907467cd725aaa6ee18bf7476
chambg02 <chambg02@gmail.com>
2022-05-06 17:38:36 +0800

    Complete project per base scope.

--- javascript.js
+++ javascript.js
@@ -3,6 +3,26 @@ const container = document.getElementById("container");
 const rows = document.getElementsByClassName("gridRow");
 const cells = document.getElementById("cellid");
 
+createGrid(100);
+
+let clear = document.getElementById("clear");
+clear.addEventListener('click', event => {
+    clearCells();
+    let choice = prompt("Choose a grid size (max. 100)", 100);
+    if (choice <= 100) {
+        createGrid(choice);
+    } else {
+        alert("That number is not valid!");
+    }
+});
+
+function clearCells() {
+    const rowsToRemove = document.querySelectorAll(".gridRow");
+    for (let c = 0; c < rowsToRemove.length; c++) {
+        container.removeChild(rowsToRemove[c]);   
+    }
+}
+
 function createGrid(size) {
     createRows(size);
     createColumns(size);
@@ -25,24 +45,17 @@ function createColumns(cellNum) {
             rows[j].appendChild(newCell);
             newCell.className = "cell";
             newCell.id = "cellid";
-            newCell.textContent = "";
         }
     }
 }
 
 function initiateHover() {
-
     const select = document.querySelectorAll("#cellid");
-    console.log(select);
-
     for (let i = 0; i < select.length; i++) {
-        select[i].addEventListener('mouseover', function() {
-            console.log("cell " + `${i}`); 
-            select[i].setAttribute("style", "background-color: black");
+        select[i].addEventListener('mouseenter', function() {
+            select[i].setAttribute("style", "background-color: #788bff");
         })
-
     }
-
 }
 
 
